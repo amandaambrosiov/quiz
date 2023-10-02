c7ce9dc7898fb548b199d610ed662b5a6a9c316a
AmandaAmbrosioUchoa <amandadanyelleuchoaambrosiocp@gmail.com>
2023-10-01 21:41:43 -0300

    mudei os botoes, os itens da tabela de informações, adcionei icones e editei os comentários

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+// criando array com o numero da questão, a questão, opçoes e resposta
+let questions = [
+    {
+    numb: 1,
+    question: "A respeito do Gerenciamento de Tempo, assinale a alternativa CORRETA.",
+    answer: "O principal objetivo dessa área é garantir que o projeto seja concluído dentro do prazo determinado.",
+    options: [
+      "Diferentemente do gerenciamento de custos, é uma área de alta visibilidade.",
+      "O cronograma do projeto é sempre uma restrição, exceto quando a data de término não é classificada como crítica",
+      "Atraso de projetos não tem influência no consumo de capital.",
+      "O principal objetivo dessa área é garantir que o projeto seja concluído dentro do prazo determinado."
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+  // voce pode comentar os codigos ou duplicar quantas vezes quiseser mas deve dar o número das series ex: 1,2,3,4,5,6....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
